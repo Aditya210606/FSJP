f35e352baa2987333bed54289b72de0aa5b3bf8b
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-02-24 23:21:02 +0530

    About comments

---
+++ Comments.java
@@ -0,0 +1,10 @@
+
+public class Comments  {
+
+    public static void main(String[] args) {
+     int number = 3 ;                  //Here we cannot use the reserve keyowrds in java 
+     System.out.println(number);  
+    }
+
+    
+}
